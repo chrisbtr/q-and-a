d4d5ca105df83ec98aecdede09935a744493bd2f
Chris <chrisbotros10@gmail.com>
2022-08-26 16:07:52 -0400

    fix: prevent search if the query only contains white spaces

--- client/Components/StackHeader.tsx
+++ client/Components/StackHeader.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Appbar, Searchbar } from "react-native-paper";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import theme from "../theme";
@@ -10,34 +10,41 @@ export interface StackHeaderProps extends NativeStackHeaderProps {
 
 const StackHeader: React.FC<StackHeaderProps> = ({
   navigation,
+  back,
 }) => {
   const [searchQuery, setSearchQuery] = React.useState("");
 
-  const hasBackButton = navigation.canGoBack();
-
   return (
     <Appbar.Header>
-      {hasBackButton ? (
+      {back ? (
         <Appbar.BackAction onPress={() => navigation.goBack()} />
       ) : null}
-      <Searchbar
-        style={styles.searchBar}
-        value={searchQuery}
-        placeholder="Search"
-        onChangeText={(text) => setSearchQuery(text)}
-        onSubmitEditing={() => {
-          navigation.push('Search', { query: searchQuery })
-        }}
-      />
+      <View style={styles.searchbarContainer}>
+        <Searchbar
+          style={styles.searchBar}
+          value={searchQuery}
+          placeholder="Search"
+          onChangeText={(text) => setSearchQuery(text)}
+          onSubmitEditing={() => {
+            const noWhitespaceQuery = searchQuery.replace(/\s/g, '');
+            if (noWhitespaceQuery !== '') {
+              navigation.push('Search', { query: searchQuery });
+            }
+          }}
+        />
+      </View>
     </Appbar.Header>
   );
 };
 
 const styles = StyleSheet.create({
+  searchbarContainer: {
+    flex: 1,
+  },
   searchBar: {
     marginLeft: 12,
-    width: "80%",
-    height: "55%",
+    marginRight: 12,
+    height: "60%",
     shadowOpacity: 0,
     backgroundColor: theme.colors.background,
   },
