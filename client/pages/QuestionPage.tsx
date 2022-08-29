5db6cf47cf07c57ae9acdd97f393a2564e803c3d
Chris <chrisbotros10@gmail.com>
2022-08-29 14:51:30 -0400

    styles: update the layout of the question and answer on the `QuestionPage`

--- client/pages/QuestionPage.tsx
+++ client/pages/QuestionPage.tsx
@@ -1,39 +1,37 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, View } from "react-native";
+import { StyleSheet, ScrollView } from "react-native";
+import { Paragraph, Title, Card } from "react-native-paper";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import { AllQuestionsStackParamList } from "./Main";
-import { Paragraph, Subheading, Title } from "react-native-paper";
 
 type QuestionPageProps = NativeStackScreenProps<AllQuestionsStackParamList, 'Question'>
 
 const QuestionPage: React.FC<QuestionPageProps> = ({ route }) => {
   const question = route.params.question;
+
   return (
-    <SafeAreaView>
-      <View>
-        <Title>{question.subject}</Title>
-        <Subheading style={styles.categorySubheading}>{question.categoryCode}</Subheading>
-        <Subheading style={styles.subheading}>Question</Subheading>
-        <Paragraph>{question.content}</Paragraph>
-        <Subheading style={styles.subheading}>Answer</Subheading>
-        <Paragraph>{question.answers?.[0]?.content || ''}</Paragraph>
-      </View>
-    </SafeAreaView>
+    <ScrollView>
+      <Card style={styles.card} mode="outlined">
+        <Card.Title title={question.subject} subtitle={question.categoryCode} titleNumberOfLines={10} />
+        <Card.Content>
+          <Paragraph>{question.content}</Paragraph>
+        </Card.Content>
+      </Card>
+      <Card style={styles.card} mode="outlined">
+        <Card.Content>
+          <Title>Answer</Title>
+          <Paragraph>{question.answers?.[0]?.content || ''}</Paragraph>
+        </Card.Content>
+      </Card>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  modal: {
-    backgroundColor: "white",
-    padding: 20,
-  },
-  subheading: {
-    textAlign: 'center',
-  },
-  categorySubheading: {
-    marginBottom: 12,
-    fontSize: 15,
+  card: {
+    marginBottom: 24,
+    borderRadius: 0,
   },
 });
 
