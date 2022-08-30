0c4bd7678c5717952106bc5cfa5a3b85195dd287
Chris <chrisbotros10@gmail.com>
2022-08-30 17:36:42 -0400

    feat: add post req for a single question

--- server/routes/questions.ts
+++ server/routes/questions.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { query } from "express-validator";
+import { query, param } from "express-validator";
 import { PrismaClient } from "@prisma/client";
 
 import validateRequest from "../middleware/validate-request";
@@ -8,61 +8,94 @@ const prisma = new PrismaClient();
 
 const router = express.Router();
 
-router.route("/").get(
-  query("id").optional().isNumeric({ no_symbols: true }),
-  query("take").optional().isNumeric({ no_symbols: true }),
-  query("skip").optional().isNumeric({ no_symbols: true }),
-  query("categoryCode").optional().isString(),
-  query("searchBy").optional().isString(),
-  validateRequest,
-  /**
-   * GET all `questions` that have been answered
-   *
-   * @param req.query.categoryCode `categoryCode` can be pass into the the request body to only
-   *    get the `questions` for that category
-   * @param req.query.take The max amount of questions send back
-   * @param req.query.skip The amount of questions to skip
-   * @param req.query.searchBy The text to filter the title of the found questions by
-   */
-  async (req, res) => {
-    try {
-      const { id, categoryCode, take = 10, skip = 0, searchBy } = req.query;
+router
+  .get(
+    "/:id",
+    /**
+     * GET a `question` that have been answered based on the question's `id`
+     *
+     * @param req.params.id the id of the question
+     */
+    async (req, res) => {
+      try {
+        const id = Number(req.params.id);
+        if (isNaN(id) || Number(id) < 0) {
+          return res.status(400).json({ message: "Invalid ID", details: {} });
+        }
+        const question = await prisma.questions.findFirst({
+          where: {
+            id: { equals: id },
+            AND: { isAnswered: { equals: true } },
+          },
+          include: { answers: true },
+        });
+        res.json(question);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error", details: {} });
+      }
+    }
+  )
+  .route("/")
+  .get(
+    query("id").optional().isNumeric({ no_symbols: true }),
+    query("take").optional().isNumeric({ no_symbols: true }),
+    query("skip").optional().isNumeric({ no_symbols: true }),
+    query("categoryCode").optional().isString(),
+    query("searchBy").optional().isString(),
+    validateRequest,
+    /**
+     * GET all `questions` that have been answered
+     *
+     * @param req.query.categoryCode `categoryCode` can be pass into the the request body to only
+     *    get the `questions` for that category
+     * @param req.query.take The max amount of questions send back
+     * @param req.query.skip The amount of questions to skip
+     * @param req.query.searchBy The text to filter the title of the found questions by
+     */
+    async (req, res) => {
+      try {
+        const { id, categoryCode, take = 10, skip = 0, searchBy } = req.query;
 
-      const count = await prisma.questions.count({ where: { isAnswered: { equals: true } } });
+        const count = await prisma.questions.count({
+          where: { isAnswered: { equals: true } },
+        });
 
-      const allQuestions = await prisma.questions.findMany({
-        where: {
-          id: { equals: Number(id) || undefined },
-          AND: {
-            categoryCode: categoryCode as string,
+        const allQuestions = await prisma.questions.findMany({
+          where: {
+            id: { equals: Number(id) || undefined },
             AND: {
-              isAnswered: { equals: true },
-              OR: [
-                {
-                  title: { contains: searchBy as string, mode: "insensitive" },
-                },
-                {
-                  content: {
-                    contains: searchBy as string,
-                    mode: "insensitive",
+              categoryCode: categoryCode as string,
+              AND: {
+                isAnswered: { equals: true },
+                OR: [
+                  {
+                    title: {
+                      contains: searchBy as string,
+                      mode: "insensitive",
+                    },
                   },
-                },
-              ],
+                  {
+                    content: {
+                      contains: searchBy as string,
+                      mode: "insensitive",
+                    },
+                  },
+                ],
+              },
             },
           },
-        },
-        take: Number(take),
-        skip: Number(skip),
-        include: { answers: true },
-      });
-      console.log(count);
-      res.json({ allQuestionsCount: count, questions: allQuestions});
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal Server Error", details: {} });
+          take: Number(take),
+          skip: Number(skip),
+          include: { answers: true },
+        });
+        res.json({ allQuestionsCount: count, questions: allQuestions });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error", details: {} });
+      }
     }
-  }
-);
+  );
 
 // TODO: Uncomment this out and fix if posting questions without answers is ever needed.
 // app.post(
